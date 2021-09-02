078838e386de105d0da1593acb83ea757658dd0b
Cihancuhlanbu <89995798+Cihancuhlanbu@users.noreply.github.com>
2021-09-02 19:45:38 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,209 @@
+const express = require("express");
+const { Client, RichEmbed } = require("discord.js");
+const app = express();
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - start > milliseconds) {
+      break;
+    }
+  }
+}
+const http = require("http");
+app.get("/", (request, response) => {
+  console.log(Date.now() + "  Token Sese Sokma | Hostlandı");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const client1 = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+const client12 = new Discord.Client();
+const client13 = new Discord.Client();
+const client14 = new Discord.Client();
+const client15 = new Discord.Client();
+const client16 = new Discord.Client();
+const client17 = new Discord.Client();
+const client18 = new Discord.Client();
+const client19 = new Discord.Client();
+const client20 = new Discord.Client();
+const client21 = new Discord.Client();
+const client22 = new Discord.Client();
+const client23 = new Discord.Client();
+const client24 = new Discord.Client();
+const client25 = new Discord.Client();
+
+//Erdem Çakıroğlu Sunar Kolay Gelsin.
+
+const data = new Map();
+
+client.login("ODgwOTA0MjU5NzgzMjMzNTg2.YSlEMg.Rpz8v9SXZRXRo98FzdX3RJoBABc"); //Hesap Tokeni Token
+
+client.on("ready", () => {
+  client.channels.get("877578813855371264").join(); //ses kanal id
+});
+
+client1.login("ODgxMTgyMDM1MTM2MzYwNDU4.YSpIZg.gG95xZaqp-uZrtkBsfdg7SmRqj0");
+
+client1.on("ready", () => {
+  client1.channels.get("877577779179302992").join();
+});
+
+client2.login("ODgxMTg1OTMwMjIyODk5Mjcy.YSpLQA.QPJazpzJi-7ik204Vi_vfA-2BAA");
+
+client2.on("ready", () => {
+  client2.channels.get("877577779179302992").join();
+});
+
+client3.login("ODUxMjMxMDg2OTE0OTYxNDU5.YL1RDg.PNHpMKKDmm7Jsr552G5oDj2hddQ");
+
+client3.on("ready", () => {
+  client3.channels.get("877578842691231755").join();
+});
+
+client4.login("ODgxMTg4NjYwNjgxMjczMzU0.YSpNTA.PZeB0VanLle1zPt4QOHZatWXpjk");
+
+client4.on("ready", () => {
+  client4.channels.get("").join();
+});
+
+client5.login("ODgxNTcyNzExOTI5OTAxMDc2.YSu3XA.n_A4JFUSBWq4UszDBCr7ngeX4NQ");
+
+client5.on("ready", () => {
+  client5.channels.get("").join();
+});
+
+client6.login("ODgxNTgwMTI5MTQ1NDU4NzM4.YSu5_g.vT1XtelvVmINwD1vzjM34pWJb2I");
+
+client6.on("ready", () => {
+  client6.channels.get("877578917664407562").join();
+});
+
+client7.login("ODgyMjU3OTcwOTcxNDM5MTE0.YS-ydg.Jbe8NcF5LbWJK_hh9kCgGruoLTc");
+
+client7.on("ready", () => {
+  client7.channels.get("877578975998787665").join();
+});
+
+client8.login("ODgyNjczNDM0ODAxMzY1MDYz.YS-z9Q.lEbkpEHPi7m8C8IPLUu3L5pdyJ4");
+
+client8.on("ready", () => {
+  client8.channels.get("877579005753192498").join();
+});
+
+client9.login("ODgyNjc0MjgwNzIxMTY2MzY3.YS-04w.6emaK-Scx1RvvI6eCyqKSwDxBa8");
+
+client9.on("ready", () => {
+  client9.channels.get("877579040339423262").join();
+});
+
+client10.login("");
+
+client10.on("ready", () => {
+  client10.channels.get("791679885835763744").join();
+});
+
+client11.login("");
+
+client11.on("ready", () => {
+  client11.channels.get("791679885835763744").join();
+});
+
+client12.login("");
+
+client12.on("ready", () => {
+  client12.channels.get("791679885835763744").join();
+});
+
+client13.login("");
+
+client13.on("ready", () => {
+  client13.channels.get("791679885835763744").join();
+});
+
+client14.login("");
+
+client14.on("ready", () => {
+  client14.channels.get("791679885835763744").join();
+});
+
+client15.login("");
+
+client15.on("ready", () => {
+  client15.channels.get("791679885835763744").join();
+});
+
+client16.login("");
+
+client16.on("ready", () => {
+  client16.channels.get("791679885835763744").join();
+});
+
+client17.login("");
+
+client17.on("ready", () => {
+  client17.channels.get("791679885835763744").join();
+});
+
+client18.login("");
+
+client18.on("ready", () => {
+  client18.channels.get("791679885835763744").join();
+});
+
+client19.login("");
+
+client19.on("ready", () => {
+  client19.channels.get("791679885835763744").join();
+});
+
+client20.login("");
+
+client20.on("ready", () => {
+  client20.channels.get("791679885835763744").join();
+});
+
+client21.login("");
+
+client21.on("ready", () => {
+  client21.channels.get("791679885835763744").join();
+});
+
+client22.login("");
+
+client22.on("ready", () => {
+  client22.channels.get("791679885835763744").join();
+});
+
+client23.login("");
+
+client23.on("ready", () => {
+  client23.channels.get("791679893788164096").join();
+});
+
+client24.login("");
+
+client24.on("ready", () => {
+  client24.channels.get("791679893788164096").join();
+});
+
+client25.login("");
+
+client25.on("ready", () => {
+  client25.channels.get("791679893788164096").join();
+});
+
+//Erdem Çakıroğlu Sunar Kolay Gelsin.//Erdem Çakıroğlu Sunar Kolay Gelsin.//Erdem Çakıroğlu Sunar Kolay Gelsin.
